19853d00f85d8969218d29e122bc466aadbd6fad
V4yukon <yxyvan@gmail.com>
2024-07-01 23:21:22 +0800

    use closure to change the program coded by for

--- currency/src/main.rs
+++ currency/src/main.rs
@@ -1,8 +1,10 @@
 use std::thread;
 use std::sync::mpsc;
-use std::time::Duration;
+use std::time::{Duration, Instant};
 
 fn main() {
+    let start = Instant::now();
+
     let (tx, rx) = mpsc::channel();
     // let handle = thread::spawn(move ||{
     //     for i in 1..10 {
@@ -18,12 +20,42 @@ fn main() {
 
 
     /*move and thread::spawn */
-    let a = vec![1, 3, 5, 7];
-    let handle = thread::spawn( move ||{
-        tx.send(a).unwrap();
+    // let a = vec![1, 3, 5, 7];
+    // let handle = thread::spawn( move ||{
+    //     tx.send(a).unwrap();
+    // });
+
+    // let rev = rx.recv().unwrap();
+
+    // println!("Got: {:?}", rev);
+    thread::spawn(move || {
+        let name_list = [
+            String::from("Blueberry"),
+            String::from("Cherry"),
+            String::from("Apple"),
+            String::from("Watermelon"),
+            String::from("Strawberry"),
+        ];
+
+        // for val in name_list {
+        //     tx.send(val).unwrap();
+        //     thread::sleep(Duration::from_millis(1));
+        // }
+        name_list
+            .into_iter()
+            .for_each(|val| {
+                tx.send(val).unwrap();
+                thread::sleep(Duration::from_millis(1))
+            });
+
     });
 
-    let rev = rx.recv().unwrap();
+    // for rev in rx {
+    //     println!("GOT: {:?}", rev);
+    // }
+    rx.into_iter()
+        .for_each(|rev| {println!("Got: {:?}", rev)});
+    let endup = start.elapsed();
+    println!("total time: {:?}", endup);
 
-    println!("Got: {:?}", rev);
 }
