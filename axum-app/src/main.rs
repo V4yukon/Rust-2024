debb00ad760ee593388bad0208b169e65143bf7a
V4yukon <yxyvan@gmail.com>
2024-07-08 23:13:19 +0800

    update a static page

--- axum-app/src/main.rs
+++ axum-app/src/main.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 use axum::extract::Query;
 use axum::response::{Html, IntoResponse};
 // use std::net::SocketAddr;
@@ -5,6 +7,7 @@ use axum::response::{Html, IntoResponse};
 use axum::Router;
 use axum::routing::get;
 use serde::Deserialize;
+use tower_http::services::ServeDir;
 
 
 #[allow(unused)]
@@ -13,9 +16,12 @@ use serde::Deserialize;
 #[tokio::main]
 
 async fn main() {
+
+    let static_file = ServeDir::new("static");
     // println!("Hello world!");
     let router_all = Router::new()
-        .merge(router_path().await);
+        .merge(router_path().await)
+        .nest_service("/static", static_file);
 
     // let addr = SocketAddr::from(([127.0.0.1], 8080));
     let addr = tokio::net::TcpListener::bind("127.0.0.1:8080").await.unwrap();
@@ -30,7 +36,9 @@ async fn main() {
 
 async fn router_path() -> Router{
     Router::new()
-        .route("/", get(hello_handler))
+        .route("/", get(root))
+        .route("/hello", get(hello_handler))
+        
 }
 
 #[derive(Debug, Deserialize)]
@@ -44,4 +52,9 @@ async fn hello_handler(Query(params): Query<HelloParams>) ->  impl IntoResponse 
     let name = params.name.as_deref().unwrap_or("World!!!");
 
     Html(format!("Hello <strong>{name}</strong>"))
+}
+
+async fn root() -> Html<String> {
+    let text_content = fs::read_to_string("static/index.html").expect("Failed to read html");
+    Html(text_content)
 }
